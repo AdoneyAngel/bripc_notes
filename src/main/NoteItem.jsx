2c060616f2771d446b47ace5bc8ac2282d83083a
Adoney Angel <Adoneyangeltj@gmail.com>
2022-11-01 20:54:40 +0000

    INSTALL: REACT-HAMMERJS
    FULLADD: Hammerjs() for mobile note press

--- src/main/NoteItem.jsx
+++ src/main/NoteItem.jsx
@@ -6,6 +6,7 @@ import ThreePoints from "../points.png"
 import bookMarkIco from "../bookmark.png"
 import checkIcon from "../check.png"
 import cloudIcon from "../cloud.png"
+import Hammer from "react-hammerjs"
 
 import NoteSettings from "./NoteSettings";
 
@@ -17,81 +18,104 @@ export default function NoteItem(props){
         noteSettingsStyles,
         notesLoading,
         settingsButtons} = props
+        
+    let hammerOptions = {
+        recognizers:{
+            press: {
+                time: 550
+            }
+        }
+    }
+
+    let hammerPress = () => {
+        setOpenSettings(true); 
+        props.setNoteSel(note);
+
+        props.setBodyClick(() => {
+            setOpenSettings(false)
+        })
+    }
+
+    let hammerPressup = () => {
+        setOpenSettings(true); 
+        props.setNoteSel(note);
+
+        props.setBodyClick(() => {
+            setOpenSettings(false)
+        })
+    }
+
 
     return (
-        <div style={{
-                            "--note-background": note.task && note.done ? "var(--task-done)" : "white",
-                            backgroundImage: "url(../bookmark.png)"
-                        }} onMouseUp={(e) => {
-                            if(e.button === 2){
-                                setOpenSettings(!openSettings) 
-                                props.setNoteSel(note)
+        <Hammer options={hammerOptions} onPress={() => hammerPress()} onPressUp={() => hammerPressup()}>
+            <div className="noteItem" style={{
+                    "--note-background": note.task && note.done ? "var(--task-done)" : "white",
+                    backgroundImage: "url(../bookmark.png)"
+                }} onMouseUp={(e) => {
+                    if(e.button === 2 && !openSettings){
+                        setOpenSettings(!openSettings) 
+                        props.setNoteSel(note)
 
-                                props.setBodyClick(() => {
-                                    setOpenSettings(false)
-                                })
+                        props.setBodyClick(() => {
+                            setOpenSettings(false)
+                        })
 
-                                setNoteSettingsStyles({
-                                    left: e.clientX,
-                                    top: e.clientY,
-                                    right: 'auto',
-                                    position: 'fixed',
-                                    animationName: "openNoteSettingsContextMenuAni"
-                                })
-                            }else{
-                                setNoteSettingsStyles({
-                                    right: '80px',
-                                    position: 'absolute',
-                                    animationName: "openNoteSettingsAni"
-                                })
-                            }
-                        }} key={note.title}>
+                        setNoteSettingsStyles({
+                            left: e.clientX,
+                            top: e.clientY,
+                            right: 'auto',
+                            position: 'fixed',
+                            animationName: "openNoteSettingsContextMenuAni"
+                        })
+                    }
+                }} key={note.title}>
+                    {
+                        openSettings && props.noteSel.title == note.title ? <NoteSettings styles={noteSettingsStyles} buttons={settingsButtons(note)} /> : null
+                    }
+                    {
+                        note.task ? <img src={bookMarkIco} alt="" /> : null
+                    }
+                    
+                    <section>
+                        <header>
+                            <h1>{note.title}</h1>
+                            <img onClick={(e) => {
+                                setOpenSettings(!openSettings); 
+                                props.setNoteSel(note);
+                                
+                                console.log(e.button)
+
+                            }} src={ThreePoints} alt="" />
+                        </header>
+                        
+                        <section>
+                            <textarea value={note.content} disabled></textarea>
+                        </section>
+
+                        <div className="underGroup">
+                            <img onClick={() => {
+                            props.openSetNoteTagDisplay()
+                            props.setNoteSel(note)
+                        }} src={note.tag ? tagBackgroundIcon : tagIcon} alt="" />
+                        
+                            <p  onClick={() => {
+                            props.openSetNoteTagDisplay()
+                            props.setNoteSel(note)
+                        }} >{note.tag}</p>
+
                             {
-                                openSettings && props.noteSel.title == note.title ? <NoteSettings styles={noteSettingsStyles} buttons={settingsButtons(note)} /> : null
-                            }
-                            {
-                                note.task ? <img src={bookMarkIco} alt="" /> : null
+                                notesLoading.indexOf(note.title) > -1 ? <img className="updatingIcon" src={cloudIcon} /> : null
                             }
                             
-                            <section>
-                                <header>
-                                    <h1>{note.title}</h1>
-                                    <img onClick={(e) => {
-                                        setOpenSettings(!openSettings); 
-                                        props.setNoteSel(note);
-                                        
-                                        console.log(e.button)
-
-                                    }} src={ThreePoints} alt="" />
-                                </header>
-                                
-                                <section>
-                                    <textarea value={note.content} disabled></textarea>
-                                </section>
-
-                                <div className="underGroup">
-                                    <img onClick={() => {
-                                    props.openSetNoteTagDisplay()
-                                    props.setNoteSel(note)
-                                }} src={note.tag ? tagBackgroundIcon : tagIcon} alt="" />
-                                
-                                    <p  onClick={() => {
-                                    props.openSetNoteTagDisplay()
-                                    props.setNoteSel(note)
-                                }} >{note.tag}</p>
-
-                                    {
-                                        notesLoading.indexOf(note.title) > -1 ? <img className="updatingIcon" src={cloudIcon} /> : null
-                                    }
-                                    
-                                    {
-                                        note.task && note.done ? <img className="checkIconTask" src={checkIcon} /> : null
-                                    }
-                                    
-                                </div>
-
-
-                            </section>
+                            {
+                                note.task && note.done ? <img className="checkIconTask" src={checkIcon} /> : null
+                            }
+                            
                         </div>
+
+
+                    </section>
+                </div>
+        </Hammer>
     )
 }
